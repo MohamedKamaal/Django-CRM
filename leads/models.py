e36865a51c088f216a397bf2d1a7016e21da9379
MohamedKamaal <mohamedkamal.analytics@gmail.com>
2025-01-02 05:38:14 +0200

    create Lead and Agent models

---
+++ leads/models.py
@@ -0,0 +1,40 @@
+""" create leads and agent models  """
+
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+class Source(models.TextChoices):
+    YOUTUBE = ("you","Youtube")
+    Google = ("go","Google")
+    NEWSLETTER = ("new","NewsLetter")
+
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    phoned = models.BooleanField(default=False)
+    source = models.CharField(max_length=10, choices = Source.choices)
+    special_files = models.FileField(null=True, blank=True)
+    agent = models.ForeignKey("Agent",on_delete = models.SET_NULL, null=True, related_name="leads")
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self):
+        return str(self.full_name)
+
+class Agent(models.Model):
+    user = models.OneToOneField("User", on_delete=models.CASCADE)    
+    image = models.ImageField(null=True,blank=True)
+    def __str__(self):
+        return f"{self.user.email}"
+
+    
+    
